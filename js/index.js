a8c022c2a1e9f24d3ab99a4dde905ca7715b3d7e
Robert Tolstov <97231830+robertproducts@users.noreply.github.com>
2022-04-02 18:28:02 +0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -5,8 +5,7 @@ $(function() {
         } else {
             $("#scroll_top").fadeOut(125);
         }
-    }
-);
+    });
 
     $("#scroll_top").click(function() {
         if (location.hash != "#header") {
@@ -18,39 +17,3 @@ $(function() {
         return false;
     });
 });
-
-function Save(theme)
-{
-    var Request = new XMLHttpRequest();
-    Request.open("GET", "./themes.php?theme=" + theme, true); //У вас путь может отличаться
-    Request.send();
-}
-
-var btn = document.getElementById("theme-button");
-var link = document.getElementById("theme-link");
-
-btn.addEventListener("click", function () { ChangeTheme(); });
-
-function ChangeTheme()
-{
-    let lightTheme = "styles/light.css";
-    let darkTheme = "styles/dark.css";
-
-    var currTheme = link.getAttribute("href");
-    var theme = "";
-
-    if(currTheme == lightTheme)
-    {
-   	 currTheme = darkTheme;
-   	 theme = "dark";
-    }
-    else
-    {    
-   	 currTheme = lightTheme;
-   	 theme = "light";
-    }
-
-    link.setAttribute("href", currTheme);
-
-    Save(theme);
-}
